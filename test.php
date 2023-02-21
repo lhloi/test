516b75c26ba69fefd876ea61f77d945c1ea371e8
loi.le <loi.le@egitech.vn>
2023-02-21 14:00:21 +0700

    a

---
+++ test.php
@@ -0,0 +1,478 @@
+{
+"orders": [
+{
+"id": 5238752870718,
+"admin_graphql_api_id": "gid://shopify/Order/5238752870718",
+"app_id": 580111,
+"browser_ip": "171.248.152.235",
+"buyer_accepts_marketing": false,
+"cancel_reason": null,
+"cancelled_at": null,
+"cart_token": "d1cedc404ab132f8b5fec1bd558d2ac8",
+"checkout_id": 36526096974142,
+"checkout_token": "924697f9ff62cb033ada70e797059ad5",
+"client_details": {
+"accept_language": "en-VN",
+"browser_height": null,
+"browser_ip": "171.248.152.235",
+"browser_width": null,
+"session_hash": null,
+"user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+},
+"closed_at": null,
+"confirmed": true,
+"contact_email": "admin@gmail.com",
+"created_at": "2022-12-30T11:41:34+07:00",
+"currency": "VND",
+"current_subtotal_price": "220000",
+"current_subtotal_price_set": {
+"shop_money": {
+"amount": "220000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "220000",
+"currency_code": "VND"
+}
+},
+"current_total_discounts": "0",
+"current_total_discounts_set": {
+"shop_money": {
+"amount": "0",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "0",
+"currency_code": "VND"
+}
+},
+"current_total_duties_set": null,
+"current_total_price": "242000",
+"current_total_price_set": {
+"shop_money": {
+"amount": "242000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "242000",
+"currency_code": "VND"
+}
+},
+"current_total_tax": "22000",
+"current_total_tax_set": {
+"shop_money": {
+"amount": "22000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "22000",
+"currency_code": "VND"
+}
+},
+"customer_locale": "en-VN",
+"device_id": null,
+"discount_codes": [],
+"email": "admin@gmail.com",
+"estimated_taxes": false,
+"financial_status": "paid",
+"fulfillment_status": null,
+"gateway": "bogus",
+"landing_site": "/",
+"landing_site_ref": null,
+"location_id": null,
+"name": "#1004",
+"note": null,
+"note_attributes": [],
+"number": 4,
+"order_number": 1004,
+"order_status_url": "https://gems-demo-four.myshopify.com/69628199230/orders/970801710116f137074daff343cd1154/authenticate?key=41a3e108b8d726fa5793154a97ae323b",
+"original_total_duties_set": null,
+"payment_gateway_names": [
+"bogus"
+],
+"phone": null,
+"presentment_currency": "VND",
+"processed_at": "2022-12-30T11:41:32+07:00",
+"processing_method": "direct",
+"reference": "975de60a1fa73d71d664dd2a7f0aff1c",
+"referring_site": "",
+"source_identifier": "975de60a1fa73d71d664dd2a7f0aff1c",
+"source_name": "web",
+"source_url": null,
+"subtotal_price": "220000",
+"subtotal_price_set": {
+"shop_money": {
+"amount": "220000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "220000",
+"currency_code": "VND"
+}
+},
+"tags": "",
+"tax_lines": [
+{
+"price": "22000",
+"rate": 0.1,
+"title": "VAT",
+"price_set": {
+"shop_money": {
+"amount": "22000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "22000",
+"currency_code": "VND"
+}
+},
+"channel_liable": false
+}
+],
+"taxes_included": false,
+"test": true,
+"token": "970801710116f137074daff343cd1154",
+"total_discounts": "0",
+"total_discounts_set": {
+"shop_money": {
+"amount": "0",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "0",
+"currency_code": "VND"
+}
+},
+"total_line_items_price": "220000",
+"total_line_items_price_set": {
+"shop_money": {
+"amount": "220000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "220000",
+"currency_code": "VND"
+}
+},
+"total_outstanding": "0",
+"total_price": "242000",
+"total_price_set": {
+"shop_money": {
+"amount": "242000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "242000",
+"currency_code": "VND"
+}
+},
+"total_price_usd": "10.22",
+"total_shipping_price_set": {
+"shop_money": {
+"amount": "0",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "0",
+"currency_code": "VND"
+}
+},
+"total_tax": "22000",
+"total_tax_set": {
+"shop_money": {
+"amount": "22000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "22000",
+"currency_code": "VND"
+}
+},
+"total_tip_received": "0",
+"total_weight": 4000,
+"updated_at": "2022-12-30T11:41:35+07:00",
+"user_id": null,
+"billing_address": {
+"first_name": "le",
+"address1": "07 trần hưng đạt, Hương Văn, Hương Trà, Thừa Thiên Huế",
+"phone": null,
+"city": "Thừa Thiên - Huế",
+"zip": "30330",
+"province": null,
+"country": "Vietnam",
+"last_name": "hoang loi",
+"address2": "a",
+"company": null,
+"latitude": null,
+"longitude": null,
+"name": "le hoang loi",
+"country_code": "VN",
+"province_code": null
+},
+"customer": {
+"id": 6733551173950,
+"email": "admin@gmail.com",
+"accepts_marketing": false,
+"created_at": "2022-12-28T09:29:12+07:00",
+"updated_at": "2022-12-30T11:41:34+07:00",
+"first_name": "le",
+"last_name": "hoang loi",
+"orders_count": 0,
+"state": "disabled",
+"total_spent": "0.00",
+"last_order_id": null,
+"note": null,
+"verified_email": true,
+"multipass_identifier": null,
+"tax_exempt": false,
+"tags": "",
+"last_order_name": null,
+"currency": "VND",
+"phone": null,
+"accepts_marketing_updated_at": "2022-12-28T09:29:12+07:00",
+"marketing_opt_in_level": null,
+"tax_exemptions": [],
+"email_marketing_consent": {
+"state": "not_subscribed",
+"opt_in_level": "single_opt_in",
+"consent_updated_at": null
+},
+"sms_marketing_consent": null,
+"admin_graphql_api_id": "gid://shopify/Customer/6733551173950",
+"default_address": {
+"id": 8992582336830,
+"customer_id": 6733551173950,
+"first_name": "le",
+"last_name": "hoang loi",
+"company": null,
+"address1": "07 trần hưng đạt, Hương Văn, Hương Trà, Thừa Thiên Huế",
+"address2": "a",
+"city": "Thừa Thiên - Huế",
+"province": null,
+"country": "Vietnam",
+"zip": "30330",
+"phone": null,
+"name": "le hoang loi",
+"province_code": null,
+"country_code": "VN",
+"country_name": "Vietnam",
+"default": true
+}
+},
+"discount_applications": [],
+"fulfillments": [],
+"line_items": [
+{
+"id": 13688323899710,
+"admin_graphql_api_id": "gid://shopify/LineItem/13688323899710",
+"fulfillable_quantity": 1,
+"fulfillment_service": "manual",
+"fulfillment_status": null,
+"gift_card": false,
+"grams": 1000,
+"name": "Áo Khoác Cardigan Viền Xanh Nâu - XL / Xanh",
+"origin_location": {
+"id": 3762866716990,
+"country_code": "VN",
+"province_code": "",
+"name": "Shop location",
+"address1": "",
+"address2": "",
+"city": "",
+"zip": ""
+},
+"price": "70000",
+"price_set": {
+"shop_money": {
+"amount": "70000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "70000",
+"currency_code": "VND"
+}
+},
+"product_exists": true,
+"product_id": 8048893034814,
+"properties": [],
+"quantity": 1,
+"requires_shipping": true,
+"sku": "DMEO-2",
+"taxable": true,
+"title": "Áo Khoác Cardigan Viền Xanh Nâu",
+"total_discount": "0",
+"total_discount_set": {
+"shop_money": {
+"amount": "0",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "0",
+"currency_code": "VND"
+}
+},
+"variant_id": 44208785654078,
+"variant_inventory_management": "shopify",
+"variant_title": "XL / Xanh",
+"vendor": "HD",
+"tax_lines": [
+{
+"channel_liable": false,
+"price": "7000",
+"price_set": {
+"shop_money": {
+"amount": "7000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "7000",
+"currency_code": "VND"
+}
+},
+"rate": 0.1,
+"title": "VAT"
+}
+],
+"duties": [],
+"discount_allocations": []
+},
+{
+"id": 13688323932478,
+"admin_graphql_api_id": "gid://shopify/LineItem/13688323932478",
+"fulfillable_quantity": 1,
+"fulfillment_service": "manual",
+"fulfillment_status": null,
+"gift_card": false,
+"grams": 3000,
+"name": "Áo hoodie dài tay có mũ nỉ trơn unisex nam - XL / Đen",
+"origin_location": {
+"id": 3762866716990,
+"country_code": "VN",
+"province_code": "",
+"name": "Shop location",
+"address1": "",
+"address2": "",
+"city": "",
+"zip": ""
+},
+"price": "150000",
+"price_set": {
+"shop_money": {
+"amount": "150000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "150000",
+"currency_code": "VND"
+}
+},
+"product_exists": true,
+"product_id": 8051489079614,
+"properties": [],
+"quantity": 1,
+"requires_shipping": true,
+"sku": "DEMO2",
+"taxable": true,
+"title": "Áo hoodie dài tay có mũ nỉ trơn unisex nam",
+"total_discount": "0",
+"total_discount_set": {
+"shop_money": {
+"amount": "0",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "0",
+"currency_code": "VND"
+}
+},
+"variant_id": 44221833511230,
+"variant_inventory_management": "shopify",
+"variant_title": "XL / Đen",
+"vendor": "HD",
+"tax_lines": [
+{
+"channel_liable": false,
+"price": "15000",
+"price_set": {
+"shop_money": {
+"amount": "15000",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "15000",
+"currency_code": "VND"
+}
+},
+"rate": 0.1,
+"title": "VAT"
+}
+],
+"duties": [],
+"discount_allocations": []
+}
+],
+"payment_details": {
+"credit_card_bin": "1",
+"avs_result_code": null,
+"cvv_result_code": null,
+"credit_card_number": "•••• •••• •••• 1",
+"credit_card_company": "Bogus"
+},
+"payment_terms": null,
+"refunds": [],
+"shipping_address": {
+"first_name": "le",
+"address1": "07 trần hưng đạt, Hương Văn, Hương Trà, Thừa Thiên Huế",
+"phone": null,
+"city": "Thừa Thiên - Huế",
+"zip": "30330",
+"province": null,
+"country": "Vietnam",
+"last_name": "hoang loi",
+"address2": "a",
+"company": null,
+"latitude": null,
+"longitude": null,
+"name": "le hoang loi",
+"country_code": "VN",
+"province_code": null
+},
+"shipping_lines": [
+{
+"id": 4282596753726,
+"carrier_identifier": "650f1a14fa979ec5c74d063e968411d4",
+"code": "Standard",
+"delivery_category": null,
+"discounted_price": "0",
+"discounted_price_set": {
+"shop_money": {
+"amount": "0",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "0",
+"currency_code": "VND"
+}
+},
+"phone": null,
+"price": "0",
+"price_set": {
+"shop_money": {
+"amount": "0",
+"currency_code": "VND"
+},
+"presentment_money": {
+"amount": "0",
+"currency_code": "VND"
+}
+},
+"requested_fulfillment_service_id": null,
+"source": "shopify",
+"title": "Standard",
+"tax_lines": [],
+"discount_allocations": []
+}
+]
+}
+]
+}
